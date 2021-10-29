0d0f64277b9301d9714ca958f698eb783e77ecfd
olashina201 <69033389+olashina201@users.noreply.github.com>
2021-10-29 02:04:43 -0500

    images and data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
+import NavBar from "./components/NavBar";
 function App() {
   return (
     <div>
-      HELLO WORLD
+      <NavBar />
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const navbar = {
+  home: "Home",
+  about: "About",
+};
+
+export const hero = {
+    greet: "Hi there, I'm",
+    name: "Jaden Tega",
+    about: "An award-winning chef with over 12 years experience. Nulla vestibulum a lorem non luctus. Aliquam ut neque est"
+}
+
+export const about = {
+  title: "About Me",
+  content:
+    "An award-winning chef with over 12 years experience Morbi porta turpis ac risus tempor mattis. Nulla vestibulum a lorem non luctus. Aliquam ut neque est. Sed tortor neque, rutrum dapibusviverra sit amet, tristique sed justo. Nulla maximus venenatis ligula quis cursus. Nullam laoreet luctus urna at mattis.",
+};
+
+export const services ={
+    title: "Services I Offer",
+    contentOne: {
+        title: "Regular Personal Chef Services",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus."
+    },
+    contentTwo: {
+        title: "Catering",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus."
+    },
+    contentThree: {
+        title: "Cooking Classes",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus."
+    },
+    contentFour: {
+        title: "New Kitchen Setup",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus. "
+    },
+    contentFive: {
+        title: "Specialty Baking",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus."
+    },
+    contentSix: {
+        title: "Meal Planning",
+        content: "Morbi porta turpis ac risus tempor mattis. Nulla  vestibulum a lorem non luctus."
+    }
+}
+
+export const experiences = {
+    experience: [
+        {
+            
+        }
+    ]
+}
